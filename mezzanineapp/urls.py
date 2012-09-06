e1c071cbc4102620255dda28cba15f0469347e0d
Lucas Westine <westine@gmail.com>
2012-09-06 11:30:21 +0800

    add infolinks
    add infolinks to base.html
    but have postinstall error with dotcloud push

--- mezzanineapp/urls.py
+++ mezzanineapp/urls.py
@@ -6,9 +6,9 @@ from mezzanine.core.views import direct_to_template
 from mezzanine.pages.views import page
 
 
-def home_page(request):
-    request.path = "/"
-    return page(request, "home")
+#def home_page(request):
+#    request.path = "/"
+#    return page(request, "home")
 
 admin.autodiscover()
 
@@ -30,7 +30,7 @@ urlpatterns = patterns("",
     # one out.
 
     #url("^$", direct_to_template, {"template": "index.html"}, name="home"),
-	url("^$", home_page, name="home"),
+	#url("^$", home_page, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
     # ---------------------------------------------
@@ -57,7 +57,7 @@ urlpatterns = patterns("",
     # ``settings.py`` module, and delete the blog page object from the
     # page tree in the admin if it was installed.
 
-    #url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
+    url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
 
     # MEZZANINE'S URLS
     # ----------------
